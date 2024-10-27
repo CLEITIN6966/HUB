4f06d2fc8d7deec3d739c2bb799142f964c2f348
CLEITIN6966 <cleitinholeiti@gmail.com>
2024-10-27 19:27:31 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,39 +1,3 @@
-local player = game.Players.LocalPlayer
-local backpack = player:WaitForChild("Backpack")
-local character = player:WaitForChild("Character") or player.CharacterAdded:Wait()
-
-local isAutoSofaActive = false -- Variável para controlar o estado do loop
-
-local function equipCouch()
-    local couchItem = backpack:FindFirstChild("Couch")
-    if couchItem then
-        couchItem.Parent = character
-    else
-        warn("Couch não encontrado na mochila.")
-    end
-end
-
--- Função que executa o loop para equipar o sofá
-local function autoEquipSofa()
-    while isAutoSofaActive do
-        equipCouch()
-        wait(0) -- Ajuste o tempo de espera conforme necessário
-    end
-end
-
--- Evento que detecta mensagens no chat
-player.Chatted:Connect(function(message)
-    if message == ".autosofa" then
-        if not isAutoSofaActive then
-            isAutoSofaActive = true
-            autoEquipSofa() -- Inicia o loop de equipar o sofá
-        end
-    elseif message == ".unsofa" then
-        isAutoSofaActive = false -- Para o loop
-    end
-end)
-
-
 local TextChat = "carregando"
 
 pcall(function()

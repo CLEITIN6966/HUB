f458cb36190c1455b2f148edae8b449db244e860
CLEITIN6966 <cleitinholeiti@gmail.com>
2024-10-28 08:32:13 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,10 +1,3 @@
--- Carrega e executa o script da URL 1
-local url = "https://raw.githubusercontent.com/CLEITIN6966/HUB/refs/heads/main/print%2Bautochat.lua"
-local response = game:HttpGet(url, true)
-loadstring(response)()
-
-wait(0.1)
-
 -- Carrega e executa o script da URL 2
 local url = "https://raw.githubusercontent.com/CLEITIN6966/HUB/refs/heads/main/2.lua"
 local response = game:HttpGet(url, true)

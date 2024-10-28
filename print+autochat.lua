2a37144f260368ed78c0bc6f70dfb54ecb00ae5f
CLEITIN6966 <cleitinholeiti@gmail.com>
2024-10-28 08:31:14 -0300

    Update print+autochat.lua

--- print+autochat.lua
+++ print+autochat.lua
@@ -31,38 +31,3 @@ local TextChat = "use .unsofa para parar de equipar o sofa"
 pcall(function()
     game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(TextChat)
 end)
-
-local player = game.Players.LocalPlayer
-local backpack = player:WaitForChild("Backpack")
-local character = player:WaitForChild("Character") or player.CharacterAdded:Wait()
-
-local isAutoSofaActive = false -- Variável para controlar o estado do loop
-
-local function equipCouch()
-    local couchItem = backpack:FindFirstChild("Couch")
-    if couchItem then
-        couchItem.Parent = character
-    else
-        warn("Couch não encontrado na mochila.")
-    end
-end
-
--- Função que executa o loop para equipar o sofá
-local function autoEquipSofa()
-    while isAutoSofaActive do
-        equipCouch()
-        wait(0) -- Ajuste o tempo de espera conforme necessário
-    end
-end
-
--- Evento que detecta mensagens no chat
-player.Chatted:Connect(function(message)
-    if message == ".autosofa" then
-        if not isAutoSofaActive then
-            isAutoSofaActive = true
-            autoEquipSofa() -- Inicia o loop de equipar o sofá
-        end
-    elseif message == ".unsofa" then
-        isAutoSofaActive = false -- Para o loop
-    end
-end)
